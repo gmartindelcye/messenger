e584bc88c451a511b00494f9ee6a40e1c96021ea
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2022-06-17 18:25:43 -0500

    server sends and recieve

--- server.py
+++ server.py
@@ -1,11 +1,56 @@
 # Import required modules
+from concurrent.futures import thread
+from email import message
 from http import client
 import socket
 import threading
+from urllib import response
 
 HOST = '127.0.0.1'
 PORT = 6543
 LISTENER_LIMIT = 5
+MSG_MAX_LENGHT = 2048
+
+active_clients = [] # all connected users 
+
+
+# Listen from upcoming messages from a client
+def listen_for_messages(client, username):
+
+    while True:
+        response = client.recv(MSG_MAX_LENGHT).decode('utf-8')
+        if response != "":
+            final_msg = username + '~' + message
+            send_messages_to_all(final_msg)
+        else:
+            print(f"The message sent from client {username} is empty!")
+
+
+# send message to a specific client
+def send_message_to_client(client, message):
+    client.sendall(message.encode())           
+
+
+# send message from user to all users connected
+def send_messages_to_all(message):
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+
+# Client handler function
+def client_handler(client):
+    
+    # Server listen for client message with username
+    while True:
+        username = client.recv(MSG_MAX_LENGHT).decode('utf-8')
+        if username != "":
+            active_clients.append((username, client))
+            break
+        else:
+            print("Client username is empty!")
+
+    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
+
 
 # main function
 def main():
@@ -24,7 +69,12 @@ def main():
     server.listen(LISTENER_LIMIT)
 
     while True:
+
         client, address = server.accept()
         print(f"Succesfully conected to client {address[0]} {address[1]}")
+
+        threading.Thread(target=client_handler, args=(client, )).start()
+
+
 if __name__ == "__main__":
     main()
